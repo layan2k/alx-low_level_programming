bb7d89e46c8c122af390b0166b757c8949a99e67
layan2k <lesliedeeshumba@gmail.com>
2021-06-18 13:46:52 +0200

    putnumberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers between 0 to 9.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int index =48;
+
+	while (index < 58)
+	{
+		putchar(index);
+         index++;
+	}
+	putchar('\n');
+	return (0);
+}

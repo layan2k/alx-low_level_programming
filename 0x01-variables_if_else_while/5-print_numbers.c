125230f574aa3d9ab762f915907978fce9ecd99e
layan2k <lesliedeeshumba@gmail.com>
2021-06-18 12:54:41 +0200

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints the alphabets.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int index = 0;
+    while (index < 11)
+    {
+        printf("%d/", index);
+    }
+    return (0);

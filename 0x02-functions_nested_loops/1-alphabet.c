f5f3fb69e6ab834dce6042eed69b2925c561a7d0
layan2k <lesliedeeshumba@gmail.com>
2021-06-22 13:09:14 +0200

    modified

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,7 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "holberton.h"
 
 /**
- * main - Prints the alphabet.
+ * print_alphabet - Prints the alphabet in lowercase.
  *
  * Return: Always 0 (Success)
  */

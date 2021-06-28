0dcc77d363b7dbced968d947af6cf631e88c1b66
layan2k <lesliedeeshumba@gmail.com>
2021-06-28 16:58:16 +0200

    keygen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -5,20 +5,27 @@
  * main - generates keygen.
  * Return: 0 Always.
  */
+
 int main(void)
 {
-	int r = 0, c = 0;
-	time_t t;
+int pass[100];
+int i, sum, n;
+sum = 0;
+srand(time(NULL));
 
-	srand((unsigned int) time(&t));
-	while (c < 2772)
-	{
-		r = rand() % 128;
-		if ((c + r) > 2772)
-			break;
-		c = c + r;
-		printf("%c", r);
-	}
-	printf("%c\n", (2772 - c));
-	return (0);
+for (i = 0; i < 100; i++)
+{
+pass[i] = rand() % 78;
+sum += (pass[i] + '0');
+putchar(pass[i] + '0');
+
+if ((2772 - sum)-'0' < 78)
+{
+n = 2772 - sum - '0';
+sum += n;
+putchar(n + '0');
+break;
+}
+}
+return (0);
 }

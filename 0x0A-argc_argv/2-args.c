bb2aaf9b011e34dd5d21428242754f206c7eae9a
layan2k <lesliedeeshumba@gmail.com>
2021-07-12 15:51:59 +0200

    correction arguments

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -5,12 +5,11 @@
  * @argv: array that contains the program command line arguments.
  * Return: 0 - success.
  */
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
-	int i = 0;
-	for (i=0; i<argc;i++)
-	{
+	int i;
+
+	for (i = 0; i < argc; i++)
 		printf("%s\n", argv[i]);
-		return (0);
-	}
+	return (0);
 }

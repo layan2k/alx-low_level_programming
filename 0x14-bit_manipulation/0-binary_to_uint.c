ae9e65477341079e1d2bb6ddff31812fcc99d689
layan2k <lesliedeeshumba@gmail.com>
2021-08-05 11:20:05 +0200

    task 0 fix try 1

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,37 +1,31 @@
 #include "main.h"
-
 /**
- * binary_to_uint - converts a binary number to an
- * unsigned int.
- * @b: binary.
- *
- * Return: unsigned int.
+ * binary_to_uint - converts a binary number into unisgned int
+ * @b: string that contains the binary number
+ * Return: a number with unsigned int type
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int ui;
-	int len, base_two;
+	int cont = 0, i, j;
+	unsigned int sum = 0, bin = 2;
 
-	if (!b)
+	if (b == NULL)
 		return (0);
-
-	ui = 0;
-
-	for (len = 0; b[len] != '\0'; len++)
-		;
-
-	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
+	while (b[cont] != '\0')
 	{
-		if (b[len] != '0' && b[len] != '1')
-		{
+		if (b[cont] != '1' && b[cont] != '0')
 			return (0);
-		}
-
-		if (b[len] & 1)
+		cont++;
+	}
+	for (i = cont - 1, j = 0; i >= 0; i--, j++)
+	{
+		if (j == 0)
+			sum = sum + (b[i] - '0');
+		else
 		{
-			ui += base_two;
+			sum = sum + ((b[i] - '0') * bin);
+			bin = bin * 2;
 		}
 	}
-
-	return (ui);
+	return (sum);
 }

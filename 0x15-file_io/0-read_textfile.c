3566db5ce1f4c4a626402d6ff689e7241b0aed9d
layan2k <lesliedeeshumba@gmail.com>
2021-08-09 13:31:04 +0200

    holberton header file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "holberton.h"
 
 /**
  * read_textfile - reads a text file and prints the letters
--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "holberton.h"
 
 /**
  * create_file - creates a file

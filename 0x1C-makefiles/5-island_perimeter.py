fabd73ea5c771a28dfadc65f8949faab1883ab0e
layan2k <lesliedeeshumba@gmail.com>
2021-11-24 23:04:50 +0200

    pycodestyle

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Island Perimeter function"""
 
+
 def island_perimeter(grid):
     """Function that returns the p"""
     p = 0
@@ -10,14 +11,14 @@ def island_perimeter(grid):
         rcolumns = len(grid[0])
         for i in range(rrows):
             for j in range(rcolumns):
-                        if grid[i][j] == 1:
-                            if (i - 1) == -1 or grid[i - 1][j] == 0:
-                                p += 1
-                            if (i + 1) == rrows or grid[i + 1][j] == 0:
-                                p += 1
-                            if (j - 1) == -1 or grid[i][j - 1] == 0:
-                                p += 1
-                            if (j + 1) == rcolumns or grid[i][j + 1] == 0:
-                                p += 1
+                if grid[i][j] == 1:
+                    if (i - 1) == -1 or grid[i - 1][j] == 0:
+                        p += 1
+                    if (i + 1) == rrows or grid[i + 1][j] == 0:
+                        p += 1
+                    if (j - 1) == -1 or grid[i][j - 1] == 0:
+                        p += 1
+                    if (j + 1) == rcolumns or grid[i][j + 1] == 0:
+                        p += 1
 
     return p
